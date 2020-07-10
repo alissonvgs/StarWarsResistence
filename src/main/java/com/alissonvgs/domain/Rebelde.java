7ad287538996d3ba041321b55a99a7de741217fa
alissonvgs <alyssonvictor00@gmail.com>
2020-07-10 17:45:39 -0300

    Classes do domain/model criadas

---
+++ src/main/java/com/alissonvgs/domain/Rebelde.java
@@ -0,0 +1,124 @@
+package com.alissonvgs.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "rebelde")
+public class Rebelde implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "idade")
+	private Integer idade;
+	
+	@Column(name = "genero")
+	private String genero;
+	
+	@OneToOne
+	private Localizacao localizacao;
+	
+	@ManyToOne
+	private Inventario inventario;
+
+	public Rebelde() {
+	}
+
+	public Rebelde(Integer id, String nome, Integer idade, String genero, Localizacao localizacao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		this.genero = genero;
+		this.localizacao = localizacao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public Inventario getInventario() {
+		return inventario;
+	}
+
+	public void setInventario(Inventario inventario) {
+		this.inventario = inventario;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rebelde other = (Rebelde) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
