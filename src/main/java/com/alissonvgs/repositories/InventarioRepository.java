6ee89c43a0bbf5cc7d751c29ed083e927a45909b
alissonvgs <alyssonvictor00@gmail.com>
2020-07-10 17:49:46 -0300

    Repositorios dos domains criados

---
+++ src/main/java/com/alissonvgs/repositories/InventarioRepository.java
@@ -0,0 +1,9 @@
+package com.alissonvgs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissonvgs.domain.Inventario;
+
+public interface InventarioRepository extends JpaRepository<Inventario, Integer> {
+
+}
---
+++ src/main/java/com/alissonvgs/repositories/TraidorReportRepository.java
@@ -0,0 +1,9 @@
+package com.alissonvgs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissonvgs.domain.TraidorReport;
+
+public interface TraidorReportRepository extends JpaRepository<TraidorReport, Integer> {
+
+}
