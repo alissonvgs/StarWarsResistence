9e65fac6ad10be4d9de63f8b1ff5458d57921f45
alissonvgs <alyssonvictor00@gmail.com>
2020-07-10 17:44:28 -0300

    Inicio do projeto| Configurando pom e properties

---
+++ src/main/java/com/alissonvgs/StarWarsResistenceSocial.java
@@ -0,0 +1,13 @@
+package com.alissonvgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsResistenceSocial {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsResistenceSocial.class, args);
+	}
+
+}
